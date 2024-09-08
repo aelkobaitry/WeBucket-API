59ce934ee383abe63627c97dad539fc558bdb8f0
Adam Elkobaitry <todoomz@gmail.com>
2024-09-07 20:21:34 -0400

    Add CORS verification

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 """Configuration for the project."""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from passlib.context import CryptContext
 from sqlmodel import Session, SQLModel, create_engine
 
@@ -8,19 +9,30 @@ from sqlmodel import Session, SQLModel, create_engine
 class Settings:
     """Settings for the project."""
 
-    PROJECT_NAME: str = "JIA"
+    PROJECT_NAME: str = "WeBucket"
     PROJECT_VERSION: str = "1.0.0"
+
+
+app = FastAPI(title=Settings.PROJECT_NAME, version=Settings.PROJECT_VERSION)
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 config = {"database": {"url": "sqlite:///database_service/db.sqlite"}}
 
-
 engine = create_engine(
     config["database"]["url"], echo=True, connect_args={"check_same_thread": False}
 )
-
-app = FastAPI(title=Settings.PROJECT_NAME, version=Settings.PROJECT_VERSION)
-
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
